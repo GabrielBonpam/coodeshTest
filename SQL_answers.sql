2f2116c3275564667c3b8ef83613b8c9e2a4755c
GabrielBonpam <53840677+GabrielBonpam@users.noreply.github.com>
2024-11-18 07:01:54 -0300

    commit: added SQL answers

---
+++ SQL_answers.sql
@@ -0,0 +1,33 @@
+Select c.customer_id, c.first_name, c.last_name, c.email
+from customers c
+left join orders o on c.customer_id = o.customer_id
+where o.order_id is NULL;
+
+
+Select p.product_id, p.product_name, p.brand_id, p.list_price
+from products p
+left join order_items oi on p.product_id = oi.product_id
+where oi.item_id is NULL;
+
+Select p.product_id, p.product_name, p.brand_id, p.category_id, p.list_price
+from products p
+left join stocks s on p.product_id = s.product_id
+where s.quantity is null or s.quantity = 0;
+
+Select s.store_name,
+       b.brand_name
+       SUM(oi.quantity) as total_quantity_sold
+from order_items oi
+inner join products p on oi.product_id = p.product_id
+inner join brands b on p.brand_id = b.brand_id
+inner join orders o on oi.order_id = o.order_id
+inner join stores s on o.store_id = s.store_id
+Where b.brand_name = "Nome da marca aqui" -- Trocar pela variável com o nome da marca
+group by s.store_name, b.brand_name
+order by total_quantity_sold desc;
+
+
+Select s.staff_id, s.first_name, s.last_name, s.email, s.phone, s.store_id
+from staffs s
+left join orders o on s.staff_id = o.staff_id
+where o.order_id is NULL;
